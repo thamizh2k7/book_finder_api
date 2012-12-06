62284f90944f7fa9f0b65a80032176c921da197a
THAMIZH <thamizh@devbrother.com>
2012-12-06 14:17:58 +0530

    Flipkart Changes updated

--- lib/book_finder_api.rb
+++ lib/book_finder_api.rb
@@ -11,11 +11,25 @@ class BookFinder
 		#	Example :
 		#		book_details = BookFinder.flipkart(9788120305960)
 	def self.flipkart(isbn)
+		#setting the url
 		search_url="http://www.flipkart.com/search/a/all?query=#{isbn}"
+
+		#initiating the Mechanize
 		result=Hash.new
 		agent =Mechanize.new
+
+		#for following redirect
 		agent.redirect_ok
+
+		#getting the page
 		page = agent.get(search_url)
+
+		#getting the titles and author
+ 		title_div=page.search(".mprod-summary-title")
+ 		result["Book"]=title_div.search("h1").text()
+ 		result["Author"]=title_div.search("h2").text()
+
+		#storing the resluts
 		page.search(".fk-specs-type2 tr").each do |tr|
 			fk_key=tr.search(".specs-key").text()
 			fk_val=tr.search(".specs-value").text()
@@ -23,19 +37,28 @@ class BookFinder
 				result[fk_key]=fk_val
 			end
 		end
-		college = page.search(".fk-lbreadbcrumb a")[4]
-		if college
-			result['college']=college.search("span").text()
+
+		#storing the colleges from breadcrumb
+		breadcrumb = page.search(".fk-lbreadbcrumb a")
+		generals = ['Other General','Others Engineering']
+		
+		if breadcrumb.count >0
+			result['college']=breadcrumb[4].search("span").text()
+			result['stream']= breadcrumb[5].search("span").text()
+			if generals.include? (result['college'])
+				result['college']=breadcrumb[5].search("span").text()
+				result['stream']= breadcrumb[6].search("span").text()
+			end
 		end
-		stream = page.search(".fk-lbreadbcrumb a")[5]
-		if stream
-			result['stream']= stream.search("span").text()
-		end
+
+		#storing the image
 		page.search("#main-image-id").each do |img_id|
 			img_tag=img_id.search("img")
 			img_url=img_tag.attribute("src")
 			result["img_url"]=img_url.to_s
 		end
+
+		#storing the price
 		price=page.search("#fk-mprod-list-id").text()
 		if price ==""	
 			price = page.search("#fk-mprod-our-id").text()
@@ -44,6 +67,7 @@ class BookFinder
 		result["price"]=price
 		result["availability"]=page.search("#fk-stock-info-id").text()
 		result["description"]=page.search("#description").inner_html().force_encoding("UTF-8")
+		result["description"].gsub!('<a href="#">top</a>',"")
 		result
 	end
 end
